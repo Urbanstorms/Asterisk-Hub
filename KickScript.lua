d7ed6e5c2077ce533849b2b4bbd4b96f208de2bc
Urbanstorms <68940892+Urbanstorms@users.noreply.github.com>
2022-05-01 15:23:50 -0400

    Create KickScript.lua

---
+++ KickScript.lua
@@ -0,0 +1,12 @@
+pcall(function()
+game.Players.LocalPlayer:Kick([[
+ㅤ
+ㅤ
+Join the Discord for a new key!
+(It's also been copied if your exploit supports it!)
+https://discord.gg/NZpS6ugu8X
+
+(If its not your key its because this game is unsupported!)
+]])
+setclipboard("https://discord.gg/NZpS6ugu8X")
+end)
