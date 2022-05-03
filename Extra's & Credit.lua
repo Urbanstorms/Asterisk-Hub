5fbdae3cac9cc857b90e9cc5630514327e3ac2fa
Urbanstorms <68940892+Urbanstorms@users.noreply.github.com>
2022-05-03 13:10:41 -0400

    Create Extra's & Credit.lua

---
+++ Extra's & Credit.lua
@@ -0,0 +1,28 @@
+local Credits = Menu.New({
+    Title = "Credits"
+})
+
+local A = Credits.Button({
+    Text = "Urbanstorm#2189 | Scriper | Urban Hub Creator | Daddy",
+    Callback = function()
+        pcall(function()
+            setclipboard("https://discord.gg/NZpS6ugu8X")
+        end)
+    end
+})
+local A = Credits.Button({
+    Text = "My Discord: https://discord.gg/NZpS6ugu8X",
+    Callback = function()
+        pcall(function()
+            setclipboard("https://discord.gg/NZpS6ugu8X")
+        end)
+    end
+})
+local A = Credits.Button({
+    Text = "Click to here copy discord link.",
+    Callback = function()
+        pcall(function()
+            setclipboard("https://discord.gg/NZpS6ugu8X")
+        end)
+    end
+})
