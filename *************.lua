bcef525219bd5aa533c893ba7ef1c9d1df15767a
Urbanstorms <68940892+Urbanstorms@users.noreply.github.com>
2022-05-11 17:40:21 -0400

    Update *************.lua

--- *************.lua
+++ *************.lua
@@ -1,8 +1,9 @@
 Keys_Table = {
     ["Kool Kids Klub"] = false, -- Expired: 5/1/2022
     ["NEWUI!"] = false, -- Expired 5/4/2022
-    ["REAPER2!"] = true,
-    ["TestKey"] = true --Urban's perma key, you can ofc use this but I'll probably change it if I see its use from time to time.
+    ["REAPER2!"] = false, --Expired 5/11/2022
+    ["NanoIsCool!"] = true,
+    ["UrbanCool"] = true --Urban's perma key, you can ofc use this but I'll probably change it if I see its use from time to time.
 }
 
 return Keys_Table
