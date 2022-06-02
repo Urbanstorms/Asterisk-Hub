f413e5948c5ff923fdce7680679e538a315c693f
Urbanstorms <68940892+Urbanstorms@users.noreply.github.com>
2022-06-01 21:33:14 -0400

    Update *************.lua

--- *************.lua
+++ *************.lua
@@ -6,7 +6,8 @@ Keys_Table = {
     ["MoreGamesSoon!"] = false, --Expired 5/18/2022
     ["GiveMeSuggestions!"] = false, --Expired 5/23/2022
     ["StrongPiece"] = false, --Expired 5/25/2022
-    ["SuggestSomeGames!"] = true,
+    ["SuggestSomeGames!"] = false, --Expired 6/1/2022
+    ["NZpS6ugu8X"] = true,
     
     ["UrbanCools"] = true --Urban's perma key, you can ofc use this but I'll probably change it if I see its use from time to time.
 }
