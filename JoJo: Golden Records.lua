4e209d9eaf4b418f65f1dbeae8db645183accf15
Urbanstorms <68940892+Urbanstorms@users.noreply.github.com>
2022-06-20 21:06:56 -0400

    Update JoJo: Golden Records.lua

--- JoJo: Golden Records.lua
+++ JoJo: Golden Records.lua
@@ -57,7 +57,7 @@ end
 
 -----Functions end
 
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Urbanstorms/UrbanHub/main/Lib.lua"))()
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Urbanstorms/Asterisk-Hub/main/Loader.Lua"))()
 local Menu = Material.Load({
     Title = "Asterisk Hub | JoJo: Golden Records",
     Style = 1,
